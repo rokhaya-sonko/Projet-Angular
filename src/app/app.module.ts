bfb1573c2db28ebaedffd640bd504568b5ae05bf
rokhaya.sonko <rokhaya.sonko@gmail.com>
2018-04-14 03:53:54 -0400

    Update github page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +16,10 @@ import { MeteoComponent } from './meteo/meteo.component';
     MeteoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
