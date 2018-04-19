3d75c501546f5da75c370933060a83e3723db719
rokhaya.sonko <rokhaya.sonko@gmail.com>
2018-04-19 14:06:49 -0400

    add services

--- src/app/mock-meteo.ts
+++
@@ -1,7 +0,0 @@
-import { Town } from './town';
-
-export const METEOS: Town[] = [
-  { id: 11, name: 'Paris', icon: string, temperature: number },
-  { id: 12, name: 'Belfort',  icon: string, temperature: number},
-  { id: 13, name: 'Compiègne', icon: string, temperature: number}
-];
