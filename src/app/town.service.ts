b265a876541227f158ccb2bbd8352cc414ddd3a5
rokhaya.sonko <rokhaya.sonko@gmail.com>
2018-04-19 18:46:10 -0400

    Services

--- src/app/town.service.ts
+++ src/app/town.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 import {  Town } from './town';
+import { MessageService } from './message.service';
+
 
 @Injectable()
 export class TownService {
 
  constructor(private messageService: MessageService) { }
  
-  getHeroes(): Observable<Hero[]> {
+  getMeteo(): Observable<Town[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('TownService: fetched meteo');
-    return of();
+    return ();
   }
 }
